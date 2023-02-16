df6419193c7c861375ef30cbf63f35f54162c6b7
hd-David <mwewahoby@gmail.com>
2023-02-16 20:20:02 +0000

    fixing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
- * main - main block
- * Return: 1
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
 */
-int main(void)
+int main()
 {
-int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-write(2,"and that piece of art is useful - Dora Korpar, 2015-10-19\n",s);
-return (1);
+const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, msg, sizeof(msg)-1);
+return 1;
 }

6d6cd382c14fec0df4e37ccfd1ebfe405758f075
hd-David <mwewahoby@gmail.com>
2023-02-16 20:46:14 +0000

    fixing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
-*/
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n";
-write 7u(2, msg, sizeof(msg)-1);
- return (1);
+    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    write(2, msg, sizeof(msg) - 1);
+    return (1);
 }

2a69b4e2b8b97bcc85ff75d522d7a395a6d270ab
hd-David <mwewahoby@gmail.com>
2023-02-16 20:25:10 +0000

    fixing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
 */
-int main()
+int main(void)
 {
 const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, msg, sizeof(msg)-1);

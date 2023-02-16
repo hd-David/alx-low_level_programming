5fb5b40171f3a70f3a8d0a0b6c4ae5c4c4331e7e
hd-David <mwewahoby@gmail.com>
2023-02-16 20:39:25 +0000

    fixing error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, msg, sizeof(msg)-1);
-return 1;
+const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n";
+write 7u(2, msg, sizeof(msg)-1);
+ return (1);
 }

5a094a7c9ba3b3487f439ed6c883f70f13e149cd
hd-David <mwewahoby@gmail.com>
2023-02-21 22:29:23 +0000

    Adding 11-print_to_98 file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /*
- * print_to_98 - prints all natural numbers from n to 98, followed by a new line.
+ * print_to_98 - prints all natural numbers
+ * from n to 98, followed by a new line.
  * @n: the number to start counting from.
  *
  * Return: void.

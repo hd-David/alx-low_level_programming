85a34fdf3cec88ef7ac1d136f92e600a9bffbf99
hd-David <mwewahoby@gmail.com>
2023-02-21 23:13:14 +0000

    finds and prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,17 +5,18 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int main()
 {
-unsigned long int a = 1, b = 2, c, i;
-printf("%lu, %lu", a, b);
-for (i = 3; i <= 98; i++)
-{
+int n = 98;
+int a = 1, b = 2;
+int i, c;
+printf("%d, %d", a, b);
+for (i = 3; i <= n; i++) {
 c = a + b;
-printf(", %lu", c);
+printf(", %d", c);
 a = b;
 b = c;
 }
 printf("\n");
-return (0);
+return 0;
 }

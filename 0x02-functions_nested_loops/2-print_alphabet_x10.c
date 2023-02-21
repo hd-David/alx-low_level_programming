e94794a83ea112b6a7fae19f8538c9a82a9f4b19
hd-David <mwewahoby@gmail.com>
2023-02-21 18:15:45 +0000

    Fixing betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -17,6 +17,9 @@ void print_alphabet_x10(void)
 int i;
 for (i = 0; i < 10; i++)
 {
+/**                                                                                                  
+ * print_alphabet - prints the alphabet in lowercase                                    
+*/
 print_alphabet();
 }
 }

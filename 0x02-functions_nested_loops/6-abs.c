3d6e4023d43d4a9b99a0302411bf69214c60c8dc
hd-David <mwewahoby@gmail.com>
2023-02-22 11:45:16 +0000

    Deleted 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value
+ * of an integer.
+ *
+ * @i: input number as an integer.
+ * Description: n is the value passed from the prototype
+ * Return: absolute value n
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (n * -1);
+}
+}

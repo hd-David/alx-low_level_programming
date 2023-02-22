62d565ba24039267b2342fda0ca545b11fc47f66
hd-David <mwewahoby@gmail.com>
2023-02-22 12:05:27 +0000

    Checking betty style

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,19 +5,22 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-int n = 98;
-int a = 1, b = 2;
-int i, c;
+int main() {
+int a = 1;
+int b = 2;
+int c;
+int count = 0;
 printf("%d, %d", a, b);
-for (i = 3; i <= n; i++)
+count += 2;
+while (count < 98)
 {
 c = a + b;
 printf(", %d", c);
 a = b;
 b = c;
+count++;
 }
 printf("\n");
-return (0);
+return 0;
 }
+

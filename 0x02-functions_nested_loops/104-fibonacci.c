66bdcee19b69caf0d0bcb08bafead2a3415824d2
hd-David <mwewahoby@gmail.com>
2023-02-22 12:53:05 +0000

    Fixing errors

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers.
+ * main - Prints the first 98 Fibonacci numbers
  *
  * Return: Always 0.
  */
 int main(void)
 {
-int count = 2;
-unsigned  long a = 1, b = 2, c;
-printf("1, 2, ");
-while (count < 98)
+unsigned long a = 1, b = 2, c;
+int i;
+printf("%lu, %lu", a, b);
+for (i = 0; i < 96; i++)
 {
 c = a + b;
+printf(", %lu", c);
 a = b;
 b = c;
-if (c > 0)
-{
-printf("%llu", c);
-if (count < 97)
-{
-printf(", ");
-}
-count++;
-}
 }
 printf("\n");
 return (0);

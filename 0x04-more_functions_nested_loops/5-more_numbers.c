69f6a2e95102477beebd83a2fba9ad5a2b6cbab4
hd-David <mwewahoby@gmail.com>
2023-02-23 12:03:41 +0000

    Adding 5-more numbers file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_x14 - prints numbers from 0 to 14, followed by a newline
+ *
+ * Return: void
+ */
+
+void print_x14(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+printf("%d", j);
+}
+printf("\n");
+}
+}

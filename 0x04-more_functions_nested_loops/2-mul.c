b4e1e10d0315b5fab8f7aea06940f5854711442a
hd-David <mwewahoby@gmail.com>
2023-02-23 10:01:41 +0000

    Adding Betty style

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
+/**
+ * mul - multiplies two integers.
+ * @a: first number.
+ * @b: second number.
+ * Return: multiplication.
+ */
 
 int mul(int a, int b)
 {
-
-  int results = a*b;
-  return results;
+return (a*b);
 }

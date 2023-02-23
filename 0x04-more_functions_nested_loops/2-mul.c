97ab87ccc8872318095486f7ce5936ce4f77efb1
hd-David <mwewahoby@gmail.com>
2023-02-23 09:17:56 +0000

    Adding gitigone file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+
+int mul(int a, int b)
+{
+
+  int results = a*b;
+  return results;
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void print_numbers(void);
+void positive_or_negative(int i);
+int mul(int a, int b);
+int _isdigit(int c);
+
+#endif

15bcf38cf9798a1851d35eac9e5065f908159e8b
hd-David <mwewahoby@gmail.com>
2023-02-23 13:21:31 +0000

    Fixing betty style

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,17 +7,17 @@
 int main(void)
 {
 int i;
-for (i=1; i<=100; i++)
+for (i = 1; i <= 100; i++)
 {
-if(i % 3 ==0 && i % 5 == 0)   
+if (i % 3 == 0 && i % 5 == 0)   
 {
 printf("FizzBuzz");
 }
-else if( i % 3 ==0)
+else if (i % 3 == 0)
 {
 printf("Fizz");
 }
-else if(i % 5 ==0)
+else if (i % 5 == 0)
 {
 printf("Buzz");
 }
@@ -27,5 +27,6 @@ printf("%d ", i);
 }
 }
 printf("\n");
+return (0);
 }
    

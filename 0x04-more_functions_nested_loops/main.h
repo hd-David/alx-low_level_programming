12a3cc87c31f6da9e9de27cff338a971adaa8605
hd-David <mwewahoby@gmail.com>
2023-02-23 19:07:00 +0000

    Printing diagonal

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,7 @@
 #include <unistd.h>
 void print_x14(void);
 void print_most_numbers(void);
-
+void print_diagonal(int n);
 void print_square(int size);
 int _putchar(char c);
 void print_numbers(void);

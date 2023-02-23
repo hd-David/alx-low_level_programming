ce1d3a296088145be4feb6714d8d7b411d5d131c
hd-David <mwewahoby@gmail.com>
2023-02-23 10:04:49 +0000

    Adding comments using betty style

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,14 @@
 #include "main.h"
-
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
 void print_numbers(void)
 {
-  int i;
-
-  for (i = 0; i <= 9; i++)
-    {
-      _putchar(i + '0');
-    }
-  _putchar('\n');
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
 }

8d6c30eee05a192ddf51fb07469e7373948ad00a
hd-David <mwewahoby@gmail.com>
2023-02-23 21:46:49 +0000

    fixing betty style

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
 * main - causes an infinite loop
-* Description: Checking what is causing the infinite loop
 * Return: 0
 */
 
@@ -10,12 +9,11 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-/**
- * while (i < 10)
- * Description: Commenting the code causing the infinite loop
- * {
- * putchar(i);
- * }
+/*
+  while (i < 10)
+  {
+  putchar(i);
+  }
  */
 printf("Infinite loop avoided! \\o/\n");
 return (0);

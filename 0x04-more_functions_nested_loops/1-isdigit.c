fe18dd8fdc2f447a0cb8a994b63baa80cf58c7f1
hd-David <mwewahoby@gmail.com>
2023-02-23 09:27:04 +0000

    Adding betty style

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,21 @@
 #include "main.h"
 
+/**
+ * Checks the character is a digit or not
+ * @c: Input number as an integer
+ *
+ * Return: Value of 1 or 0
+ */
+
 int _isdigit(int c)
 
-
 {
-  if(c>= '0' && c<= '9')
-    {
-      return 1;
-      
-    }
-  else
-    {
-      return 0;
-    } 
+if(c>= '0' && c<= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+} 
 }

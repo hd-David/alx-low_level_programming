a9300514f00c258c919da25bda3095107ae5d447
hd-David <mwewahoby@gmail.com>
2023-02-23 12:39:24 +0000

    Adding printing triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+/**
+ * print_triangle - prints a triangle followed by a new line
+ * @size: the size of the triangle to print
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+int row, col;
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (row = 0; row < size; row++)
+{
+for (col = 0; col < size; col++)
+{
+if (col < size - row - 1)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,5 +10,6 @@ void print_numbers(void);
 void positive_or_negative(int i);
 int mul(int a, int b);
 int _isdigit(int c);
+void print_triangle(int size);
 void more_numbers(void);
 #endif

f6b1158c6ec20f43dbfe651e9d79035e9fc2e087
hd-David <mwewahoby@gmail.com>
2023-03-02 06:50:26 +0000

    Adding solution for 0x2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - copies strings
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * @n: maximum number of characters from src to be used
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+/*copy up to n characters from src to dest*/
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i],
+}
+/* Add null bytes if n > strlen(src) */
+while (i < n)
+{
+dest[i] = '\0';
+i++
+}
+return (dest);
+}

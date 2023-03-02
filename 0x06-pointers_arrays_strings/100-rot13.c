d12e305b3f98578b9941b671bad5804fd5af6d64
hd-David <mwewahoby@gmail.com>
2023-03-02 09:45:40 +0000

    Updating headers file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - Encodes a string using rot13
+ * @s: Pointer to the string to encode
+ *
+ * Return: Pointer to the encoded string
+ */
+char *rot13(char *s)
+{
+int i, j;
+char *alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char *rot13 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; alphabet[j] != '\0'; j++)
+{
+if (s[i] == alphabet[j])
+{
+s[i] = rot13[j];
+break;
+}
+}
+}
+
+return s;
+}

32f2e1ae6f582ca9384d3b34d2a123daec3a28e9
hd-David <mwewahoby@gmail.com>
2023-03-02 10:19:01 +0000

    Updating headers file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-
+int _putchar(char c);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);

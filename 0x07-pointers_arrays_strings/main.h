677084ac4fd55154682c4f722809933f8e6f93d7
hd-David <mwewahoby@gmail.com>
2023-03-06 20:29:22 +0000

    Adding solution for 0x8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,6 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+unsigned int _strspn(char *s, char *accept);
+char *_strchr(char *s, char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);

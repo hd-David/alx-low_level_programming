a32928014369ecb2b09e0903fce5bcb0f6a28930
hd-David <mwewahoby@gmail.com>
2023-03-06 20:16:01 +0000

    Adding solution for 0x7

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+void print_chessboard(char (*a)[8]);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
+void print_diagsums(int *a, int size);
+char *_strstr(char *haystack, char *needle);
 #endif /* MAIN_H */

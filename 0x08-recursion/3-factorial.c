cf5295262fcd132a8508fda723b47cd6db70c00f
hd-David <mwewahoby@gmail.com>
2023-03-08 14:58:34 +0000

    Adding solution for 0x4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a number
+ * @n: input number
+ *
+ * Return: factorial of n, -1 if n is negative
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+else
+{
+return (n * factorial(n - 1));
+}
+}

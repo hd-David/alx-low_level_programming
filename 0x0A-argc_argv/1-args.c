90d558a92a3ba731bbd0160de66b8ea4ac23142e
hd-David <mwewahoby@gmail.com>
2023-03-10 18:55:01 +0000

    Adding solution for 0x1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: number of cl arguments passed to the program
+ * @argv: an array of strings containing the cl arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+printf("Number of arguments: %d\n", argc);
+return (0);
+}

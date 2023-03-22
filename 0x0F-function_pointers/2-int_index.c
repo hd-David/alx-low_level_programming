e9470a3662d245ca371d3719774a73ef699a0e7f
hd-David <mwewahoby@gmail.com>
2023-03-22 18:59:41 +0000

    Some tests are failing

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,10 +13,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int k;
 
-if (size <= 0)
-return (-1);
+
 if (array && cmp)
 {
+if (size <= 0)
+return (-1);
 for (k = 0; k < size; k++)
 {
 if (cmp(array[k]) != 0)

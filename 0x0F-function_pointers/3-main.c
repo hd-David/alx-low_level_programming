2a19a17534e66de9dccecc4d89ef9112f501c5d7
hd-David <mwewahoby@gmail.com>
2023-03-22 20:40:16 +0000

    Adding solution for 0x3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: the number of command-line arguments
+ * @argv: an array of command-line argument strings
+ *
+ * Return: 0 if successful, otherwise 98 if there are
+ *         incorrect number of arguments, 99 if the
+ *         operator is invalid, or 100 if there is an
+ *         attempt to divide or mod by zero
+ */
+int (*get_op_func(char *s))(int, int);
+
+
+int main(int argc, char **argv)
+{
+int a, b, result;
+int (*op_func)(int, int);
+
+if (argc != 4)
+{
+printf("Error: Incorrect number of arguments\n");
+return (98);
+}
+
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+
+if (b == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+{
+printf("Error: Cannot divide or mod by zero\n");
+return (100);
+}
+
+op_func = get_op_func(argv[2]);
+
+if (!op_func)
+{
+printf("Error: Operator not recognized\n");
+return (99);
+}
+
+result = op_func(a, b);
+printf("%d\n", result);
+
+return (0);
+}

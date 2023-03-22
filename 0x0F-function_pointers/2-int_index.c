1e509996d8cb338e70ce2ed3b4a20f549299864b
hd-David <mwewahoby@gmail.com>
2023-03-22 18:53:42 +0000

    Align with betty style

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,11 +8,11 @@
  * Return: index of the first element for which the cmp function does
  * not return 0,
  * or -1 if no element matches or size <= 0.
- */
+*/
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int k;
-  
+
 if (size <= 0)
 return (-1);
 if (array && cmp)

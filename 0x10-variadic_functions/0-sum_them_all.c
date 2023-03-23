be1f04cba3c8b2f5a53aa44832d72c680b6013f9
hd-David <mwewahoby@gmail.com>
2023-03-23 14:47:57 +0000

    First commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - finds the sum of all passed args
+ *@n: is the first args and used to find how args passed to the function
+ *
+ *Return: sum of the numbers pased to the functions.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+va_list args;
+ int sum = 0, num;
+
+if (n == 0)
+return (0);
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+{
+num = va_arg(args, int);
+sum += num;
+
+}
+va_end(args);
+return (sum);
+}

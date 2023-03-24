9dd6a4739f6b9d3c70a8d9e933e899d934b478a2
hd-David <mwewahoby@gmail.com>
2023-03-24 18:19:50 +0000

    fixing errors

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,60 +3,47 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-
 void print_all(const char * const format, ...)
 {
-    int i = 0;
-    char c = ' ';
-    float f = 0.0;
-    char *s = NULL;
-
-    va_list args;
-    va_start(args, format);
-    
-    while (format && format[i])
-    {
-        switch (format[i])
-        {
-            case 'c':
-                c = va_arg(args, int);
-                printf("%c", c);
-                break;
-
-            case 'i':
-                i = va_arg(args, int);
-                printf("%d", i);
-                break;
-
-            case 'f':
-                f = va_arg(args, double);
-                printf("%f", f);
-                break;
-
-            case 's':
-                s = va_arg(args, char*);
-                if (s == NULL)
-                {
-                    printf("(nil)");
-                }
-                else
-                {
-                    printf("%s", s);
-                }
-                break;
-
-            default:
-                break;
-        }
-
-        if (format[i + 1])
-        {
-            printf(", ");
-        }
-
-        i++;
-    }
-
-    printf("\n");
-    va_end(args);
+va_list valist;
+unsigned int i = 0, j, c = 0;
+char *str;
+const char t_arg[] = "cifs";
+va_start(valist, format);
+while (format && format[i])
+{
+j = 0;
+while (t_arg[j])
+{
+if (format[i] == t_arg[j] && c)
+{
+printf(", ");
+break;
+}
+j++;
+}
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(valist, int)), c = 1;
+break;
+case 'i':
+printf("%d", va_arg(valist, int)), c = 1;
+break;
+case 'f':
+printf("%f", va_arg(valist, double)), c = 1;
+break;
+case 's':
+str = va_arg(valist, char *), c = 1;
+if (!str)
+{
+printf("(nil)");
+break;
+}
+printf("%s", str);
+break;
+}
+i++;
+}
+printf("\n"), va_end(valist);
 }

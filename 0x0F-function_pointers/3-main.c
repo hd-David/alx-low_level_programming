2b8c4805db402f68bff9cba30e734d069c2b140e
hd-David <mwewahoby@gmail.com>
2023-03-24 07:22:18 +0000

    Fixing errors

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,49 +1,42 @@
 #include <stdlib.h>
 #include "3-calc.h"
 #include <stdio.h>
+
 /**
- * main - entry point
- * @argc: the number of command-line arguments
- * @argv: an array of command-line argument strings
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- * Return: 0 if successful, otherwise 98 if there are
- *         incorrect number of arguments, 99 if the
- *         operator is invalid, or 100 if there is an
- *         attempt to divide or mod by zero
+ * Return: Always 0.
  */
-int (*get_op_func(char *s))(int, int);
-
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int a, b, result;
-int (*op_func)(int, int);
+int a, b;
+int (*operation)(int, int);
 
 if (argc != 4)
 {
-printf("Error: Incorrect number of arguments\n");
-exit (98);
+printf("Error\n");
+exit(98);
+}
+
+if (argv[2][1])
+{
+printf("Error\n");
+exit(99);
+}
+
+operation = get_op_func(argv[2]);
+
+if (operation == NULL)
+{
+printf("Error\n");
+exit(99);
 }
 
 a = atoi(argv[1]);
 b = atoi(argv[3]);
 
-if (b == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
-{
-printf("Error: Cannot divide or mod by zero\n");
-exit (99);
-}
-
-op_func = get_op_func(argv[2]);
-
-if (!op_func)
-{
-printf("Error: Operator not recognized\n");
-exit (99);
-}
-
-result = op_func(a, b);
-printf("%d\n", result);
-
+printf("%d\n", operation(a, b));
 return (0);
 }

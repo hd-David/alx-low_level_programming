0403ecbdfe0d05d95553988bc612a61d207bd39d
hd-David <mwewahoby@gmail.com>
2023-03-31 22:36:13 +0000

    Fixing betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
-
-#include <stdio.h>
-
-
+/**
+ * before_main -  prints the massage before main
+ * Description: the function prints before main
+ */
 void __attribute__ ((constructor)) before_main(void)
 {
 printf("You're beat! and yet, you must allow,\n"
-           "I bore my house upon my back!\n");
+"I bore my house upon my back!\n");
 }

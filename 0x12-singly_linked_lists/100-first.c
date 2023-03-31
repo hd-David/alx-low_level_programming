fc32f7837d5288bc774772da80c218d4ca0dfe82
hd-David <mwewahoby@gmail.com>
2023-03-31 22:27:37 +0000

    Adding solution for 0x5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+#include <stdio.h>
+
+
+void __attribute__ ((constructor)) before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+           "I bore my house upon my back!\n");
+}

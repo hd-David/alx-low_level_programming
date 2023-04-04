ee34054aceede466d6a5225527382ab45360f8fa
hd-David <mwewahoby@gmail.com>
2023-04-04 13:25:28 +0000

    Adding solution for 0x4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 
 /**
- * free_listint2 - This frees listint_t list
+ * free_listint - this frees listint_t list
  *
  * @head: ppointer to the head of the list
  *
- * Return: nothing
+ * Description: Frees all the nodes of a linked list.
+ *
  */
 
 void free_listint(listint_t *head)
 {
 listint_t *curent_node = head, *next_node;
 
-while(curent_node != NULL)
+while (curent_node != NULL)
 {
 next_node = curent_node->next;
-     
 free(curent_node);
-curent_node = next_node;     
+curent_node = next_node;
+
 }
 
 }

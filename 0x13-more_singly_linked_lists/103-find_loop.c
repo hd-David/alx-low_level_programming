b9884f1a34b95a42e6a634df643e29695b2594aa
hd-David <mwewahoby@gmail.com>
2023-04-05 13:33:54 +0000

    Adding solution for 0xE

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * find_listint_loop - Finds the loop in a linked list.
+ * @head: Pointer to the head of the linked list.
+ *
+ * Return: The address of the node where the loop starts,
+ *         or NULL if there is no loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+listint_t *slow_p = head, *fast_p = head;
+
+while (slow_p && fast_p && fast_p->next)
+{
+slow_p = slow_p->next;
+fast_p = fast_p->next->next;
+
+if (slow_p == fast_p)
+	{
+slow_p = head;
+
+while (slow_p && fast_p)
+	    {
+if (slow_p == fast_p)
+return (slow_p);
+
+slow_p = slow_p->next;
+fast_p = fast_p->next;
+}
+}
+}
+
+return (NULL);
+}

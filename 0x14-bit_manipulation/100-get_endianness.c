edd5c37b6e60af6b9fa9bd41b1c1194b475eb14e
hd-David <mwewahoby@gmail.com>
2023-04-10 11:32:43 +0000

    Updating header file

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness of the machine.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+unsigned int num = 1;
+char *c = (char *)&num;
+
+if (*c)
+return(1); /* little endian */
+else
+return (0); /* big endian */
+}
